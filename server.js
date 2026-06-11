4e026a2ad2e53800ba05d451b0c83e59ccd8b07e
Yoga <kebookanjut@gmail.com>
2026-06-11 16:21:50 +0700

    render port fix

--- server.js
+++ server.js
@@ -41,10 +41,13 @@ mqttClient.on(
     }
 );
 
-server.listen(3000, () => {
+const PORT =
+    process.env.PORT || 3000;
+
+server.listen(PORT, () => {
 
     console.log(
-        'Web Server Running'
+        `Web Server Running on ${PORT}`
     );
 
 });
